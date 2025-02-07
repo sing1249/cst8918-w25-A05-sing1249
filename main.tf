ff0c651f05deb374547ba79eb627f5dd432a0db0
sing1249 <sing1249@algonquinlive.com>
2025-02-07 13:02:06 -0500

    Typo fix

--- main.tf
+++ main.tf
@@ -106,6 +106,7 @@ resource "azurerm_network_interface" "nic" {
 resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
+ }
 }
 
 
