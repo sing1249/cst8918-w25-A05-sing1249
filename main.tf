91f856791f81e83f726ff73ca32dba3ee98df3af
sing1249 <sing1249@algonquinlive.com>
2025-02-07 12:35:51 -0500

    Define Virtual Machine

--- main.tf
+++ main.tf
@@ -118,3 +118,29 @@ data "cloudinit_config" "webserver" {
     content      = file("${path.module}/init.sh")
   }
 }
+
+
+# Defining Virtual Machine
+resource "azurerm_linux_virtual_machine" "webserver" {
+  name                = "${var.labelPrefix}-A05-VM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("${path.module}/id_rsa.pub")
+  }
+  custom_data = data.cloudinit_config.webserver.rendered
+}
