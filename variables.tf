c4c674f5131efed8d4f2044714d843e177322f49
sing1249 <sing1249@algonquinlive.com>
2025-02-07 11:54:17 -0500

    Define Terraform input variables for resource naming and resource configuration including type as well

--- variables.tf
+++ variables.tf
@@ -1,13 +1,17 @@
 variable "labelPrefix" {
   description = "Prefix for resource naming"
+  type        = string
 }
 
 variable "region" {
   description = "Azure Region"
+  type        = string
   default     = "Canada Central"
 }
 
 variable "admin_username" {
-  description = "Admin username for the resources"
+  description = "Admin username for the VM"
+  type        = string
   default     = "azureadmin"
 }
+
