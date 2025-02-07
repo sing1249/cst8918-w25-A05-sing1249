a589781e6ab197d87e2b2da1c318514665152f25
sing1249 <sing1249@algonquinlive.com>
2025-02-07 12:09:02 -0500

    Define Security group with allowing SSH and HTTP rules

--- main.tf
+++ main.tf
@@ -56,3 +56,34 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.rg.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+# Defining Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.labelPrefix}-A05-NSG"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
